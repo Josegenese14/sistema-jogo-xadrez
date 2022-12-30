1af4f318dceeeb7db47e30afd57116f3c4d755bc
Jose Genese <jgenese6@gmail.com>
2022-12-30 18:52:29 -0300

    Possiveis movimento da peça

--- src/bordaGame/Piece.java
+++ src/bordaGame/Piece.java
@@ -1,6 +1,6 @@
 package bordaGame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected posicao position;
 	private Board board;
@@ -13,6 +13,24 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-
+	
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(posicao position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+	public boolean isThereanyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+			for (int i=0; i<mat.length; i++) {
+				for (int j=0; j<mat.length; j++) {
+					if(mat[i][j]) {
+						return true;
+					}
+				}
+			}
+		return false;
+	}
+	
 	
 }
--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -3,7 +3,7 @@ package chess;
 import bordaGame.Board;
 import bordaGame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
 	
 	private Color color;
 
